28da43b116d117c3884ce6dd742b3696bdff5551
Shivam Maheshwari <shivam@canbrand.in>
2016-07-01 21:40:12 +0530

    Renamed Bootstrap to SpeediFii and improved log support

--- code/public/index.php
+++ code/public/index.php
@@ -5,8 +5,8 @@ ini_set('display_errors','off');
 ob_start();
 
 //Require Bootstrap Class
-require realpath(__dir__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'speedifii' . DIRECTORY_SEPARATOR . 'bootstrap.class.inc');
+require realpath(__dir__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'speedifii' . DIRECTORY_SEPARATOR . 'speedifii.class.inc');
 
 //Run
-speedifii\Bootstrap::run();
+speedifii\SpeediFii::run();
 /* EoF */
